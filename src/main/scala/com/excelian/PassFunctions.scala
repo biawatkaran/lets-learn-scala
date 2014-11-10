88c9990720032962cb108bb65ce388625af98d88
Karan Biawat <biawat.karan@gmail.colm>
2014-11-10 11:41:17 +0000

    Pass functions to methods

---
+++ src/main/scala/com/excelian/PassFunctions.scala
@@ -0,0 +1,69 @@
+package com.excelian
+
+/**
+ * Created by karandeep.biawat on 10/11/2014.
+ */
+
+class Ap {
+  def apply(in : Int) = in.toString
+}
+
+class PassFunctions {
+
+  /*
+  When passing function treated as anonymous class implementing trait(interface) having apply method
+  e.g.
+    Function[A,B]
+      A - parameter type
+      B - return type
+
+      Function.apply(p: A) : B
+   */
+
+  //*******************************************************************
+  // these three methods are way to use apply method as param check in main method
+  def passFunctionsMethod1(f : Function1[Int,String])= f.apply(42)
+
+  def passFunctionsMethod2(f: Int => String) = f.apply(42)
+
+  def passFunctionsInCaseObjectHasApplyMethod(f: Int => String) = f(42)
+
+  def apply(p: Int): String = {
+    p.toString
+  }
+
+
+
+
+  //*******************************************************************
+  // pass a method that accepts nothing and returns nothing
+  def passFunctions(callback: () => Unit){
+    while(true){
+      callback()
+      Thread.sleep(1000)
+    }
+  }
+
+  def timeFlies() = {
+    println("time flies...")
+  }
+
+}
+
+object TestPassFunctions {
+  //*******************************************************************
+
+  def main(args: Array[String]) {
+    val obj1 = new Ap
+    print("Value returned :" +obj1(12) )
+
+    val obj2 = new PassFunctions
+    obj2.passFunctions(obj2.timeFlies)
+
+    obj2.passFunctionsMethod1(obj2.apply)
+    obj2.passFunctionsMethod2(obj2.apply)
+    obj2.passFunctionsInCaseObjectHasApplyMethod(obj2.apply)
+
+  }
+}
+
