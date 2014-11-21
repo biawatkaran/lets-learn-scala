92f856924adfc0b607c68f1133a5cf03d0dfb00d
Karan Biawat <biawat.karan@gmail.colm>
2014-11-21 01:17:36 +0000

    Option type

---
+++ src/main/scala/com/scala/exercise/types/option/OptionUsage.scala
@@ -0,0 +1,36 @@
+package com.scala.exercise.types.option
+
+import java.lang.{Boolean => JBool}
+/**
+ * Created by Beauty on 11/20/14.
+ */
+object OptionUsage {
+
+  def main(args: Array[String]) {
+
+    println(toInt("1"))
+
+    println(toBool("1==1"))
+  }
+
+  //*************************************
+  //********* util **********************
+  def tryo[T](f: => T) : Option[T] ={
+    try{
+      Some(f);
+    } catch {
+      case _ => None
+    }
+  }
+
+  //************** helpers ***************
+  def toInt(s: String) : Option[Int] = {
+    tryo(s.toInt)
+  }
+
+  def toBool(s: String) = {
+    tryo(JBool.parseBoolean(s))
+  }
+
+  //***************************************
+}
